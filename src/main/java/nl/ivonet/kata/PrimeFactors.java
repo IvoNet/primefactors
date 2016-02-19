70c9226cb011440f40e4d7df34e1e76ccfb84d30
IvoNet <ivonet@gmail.com>
2016-02-19 20:05:14 +0100

    step 1: test created for value 0 but failing test

--- src/main/java/nl/ivonet/kata/PrimeFactors.java
+++ src/main/java/nl/ivonet/kata/PrimeFactors.java
@@ -1,4 +1,9 @@
 package nl.ivonet.kata;
 
+import java.util.List;
+
 public class PrimeFactors {
+    public List<Integer> process(final int input) {
+        return null;
+    }
 }
--- src/test/java/nl/ivonet/kata/PrimeFactorsTest.java
+++ src/test/java/nl/ivonet/kata/PrimeFactorsTest.java
@@ -1,5 +1,24 @@
 package nl.ivonet.kata;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrimeFactorsTest {
 
+    private PrimeFactors factors;
+
+    @Before
+    public void setUp() throws Exception {
+        this.factors = new PrimeFactors();
+    }
+
+
+    @Test
+    public void test0() throws Exception {
+        assertEquals(Collections.emptyList(), factors.process(0));
+    }
 }
