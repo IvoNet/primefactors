44c69d9264e5abe4ddd911d5676c200bae6a5b25
IvoNet <ivonet@gmail.com>
2016-02-19 21:01:02 +0100

    Added test for value 4 passing....

--- src/main/java/nl/ivonet/kata/PrimeFactors.java
+++ src/main/java/nl/ivonet/kata/PrimeFactors.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 public class PrimeFactors {
     public List<Integer> process(final int input) {
+        int primefactor = input;
         final List<Integer> factors = new ArrayList<Integer>();
 
-        if (input ==  3) {
+        if (primefactor == 4) {
+            factors.add(2);
+            primefactor /= 2;
+            factors.add(2);
+            primefactor /= 2;
+        }
+        if (primefactor ==  3) {
             factors.add(3);
         }
-        if (input ==  2) {
+        if (primefactor ==  2) {
             factors.add(2);
         }
 
