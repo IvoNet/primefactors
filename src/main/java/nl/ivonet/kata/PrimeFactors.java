103d76717859f0f864e61b5c998251e91a4dd42a
IvoNet <ivonet@gmail.com>
2016-02-19 20:23:37 +0100

    step 2: test on zero but with

--- src/main/java/nl/ivonet/kata/PrimeFactors.java
+++ src/main/java/nl/ivonet/kata/PrimeFactors.java
@@ -1,9 +1,10 @@
 package nl.ivonet.kata;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactors {
     public List<Integer> process(final int input) {
-        return null;
+        return Collections.emptyList();
     }
 }
