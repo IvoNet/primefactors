e6c068e7dcbf6a16b317bef98835d9fe10afd55a
IvoNet <ivonet@gmail.com>
2016-02-19 20:55:20 +0100

    step 6: Added test for value 3 with working test

--- src/main/java/nl/ivonet/kata/PrimeFactors.java
+++ src/main/java/nl/ivonet/kata/PrimeFactors.java
@@ -7,6 +7,10 @@ public class PrimeFactors {
     public List<Integer> process(final int input) {
         final List<Integer> factors = new ArrayList<Integer>();
 
+
+        if (input ==  3) {
+            factors.add(3);
+        }
         if (input ==  2) {
             factors.add(2);
         }
